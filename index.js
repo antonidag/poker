732fb7157e7ce20f7d75947fd7c21914163b75d1
Anton Björkman <anton.bjorkman@epicalgroup.com>
2023-07-12 16:26:11 +0200

    back4app fixes

--- index.js
+++ index.js
@@ -3,8 +3,7 @@
 const express = require('express');
 
 // Constants
-const PORT = 3000;
-const HOST = 'localhost';
+const PORT = 8080;
 
 // App
 const app = express();
@@ -12,8 +11,8 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(PORT, 'localhost', () => {
-  console.log(`Running on http://localhost:3000`);
+app.listen(PORT, '0.0.0.0' , () => {
+  console.log(`Running on http://0.0.0.0:3000`);
 });
 
 
