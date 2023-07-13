3848f2ac45654b7a28a97df609700c249488d990
Anton Björkman <anton.bjorkman@epicalgroup.com>
2023-07-13 15:58:29 +0200

    add create player record api

--- airtable/index.js
+++ airtable/index.js
@@ -1,25 +1,63 @@
 export class AirTableAPIClient {
 
-    constructor(patToken){
+    constructor(patToken) {
         this.patToken = patToken
     }
 
     async getTableRecords(tableName) {
         const isString = typeof tableName == 'string' ? true : false;
-        if(!isString || tableName == undefined) {
+        if (!isString || tableName == undefined) {
             throw new Error('Missing tableName parameter or tableName parameter is undefined')
         }
 
         var myHeaders = new Headers();
         myHeaders.append("Authorization", `Bearer ${this.patToken}`);
-      
+
         var requestOptions = {
-          method: 'GET',
-          headers: myHeaders,
-          redirect: 'follow'
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
         };
         const response = await fetch(`https://api.airtable.com/v0/app73ftdOgLbYaulY/${tableName}?view=Grid%20view`, requestOptions);
         const jsonObj = await response.json();
         return jsonObj.records.map(item => item.fields);
+    }
+
+    async createTableRecordPlayer(tableName, reqBody) {
+        const isString = typeof tableName == 'string' ? true : false;
+        if (!isString || tableName == undefined) {
+            throw new Error('Missing tableName parameter or tableName parameter is undefined')
+        }
+
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", `Bearer ${this.patToken}`);
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({
+            "records": [
+                {
+                    "fields": {
+                        "FirstName": reqBody.firstName,
+                        "LastName": reqBody.lastName,
+                        "Status": reqBody.status,
+                        "img": [
+                            {
+                                "url": "https://v5.airtableusercontent.com/v1/18/18/1689264000000/bXwVgTp0eQnxwM3H_L9jow/pEKbKD7keDSGyYZt-OclWz32qMnAoQ8-CLpjpYOTmvqKyt5M3DkLusXrNEuOrHkALnDjFIXb_SnCSeRpM6OPutqFnIkH1wjaFvYEeBMDnNBWAuDw__llgqqrb55bh-wu/PVMdquTSYItvohrE4_Mt-8dxlMoFylKTdksM-pNIHlU"
+                            }
+                        ]
+                    }
+                }
+            ]
+        });
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        const response = await fetch("https://api.airtable.com/v0/app73ftdOgLbYaulY/Players", requestOptions);
+        return response.body;
     }
 }
--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 'use strict';
 
 import express from 'express';
+import bodyParser from 'body-parser';
 import dot from 'dotenv'
 import { AirTableAPIClient } from './airtable/index.js';
 dot.config()
 const PORT = 8080;
 
-const airTableAPIClient = new AirTableAPIClient(process.env['airtableKey'])
+const airTableAPIClient = new AirTableAPIClient(process.env['AIRTABLEKEY'])
 
 // App
+
 const app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send("H€¥ V¡©t()®! Jµ$t wªnt€d t() $€nd $()m€ p()$¡t¡v€ v¡b€$ ¥()µ® wª¥. ¥()µ'®€ € l¡k€ ª $h¡n¡ng $tª®, ®ªd¡ªt¡ng b®¡££¡ªn¢€ ¢ª®act€®$ ªnd mªk¡ng th€ w()®£d ª b€tt€® p£ª¢€. K€€p b€¡ng th€ ¡n¢®€d¡b£€ p€®$()n ¥");
 });
@@ -25,6 +31,19 @@ app.get('/players', async (req, res) => {
   }
 
 });
+
+app.post('/players', async (req, res) => {
+  try {
+    console.log(req.body)
+    const result = await airTableAPIClient.createTableRecordPlayer('Players',req.body);
+    res.status(200).send(result)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send({error: error})
+  }
+
+});
+
 
 app.get('/games', async (req, res) => {
   try {
