505d1301c8b487bc7d1386d6d40c6272df37f900
Anton Björkman <anton.bjorkman@epicalgroup.com>
2023-07-13 09:14:50 +0200

    victor test

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = 8080;
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send("H€¥ V¡©t()®! Jµ$t wªnt€d t() $€nd $()m€ p()$¡t¡v€ v¡b€$ ¥()µ® wª¥. ¥()µ'®€ € l¡k€ ª $h¡n¡ng $tª®, ®ªd¡ªt¡ng b®¡££¡ªn¢€ ¢ª®act€®$ ªnd mªk¡ng th€ w()®£d ª b€tt€® p£ª¢€. K€€p b€¡ng th€ ¡n¢®€d¡b£€ p€®$()n ¥");
 });
 
 app.listen(PORT, '0.0.0.0' , () => {
