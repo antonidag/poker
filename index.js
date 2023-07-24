d2bd2ebeb9bfa4cb995b542e531e12af12bacbae
Anton Björkman <anton.bjorkman@epicalgroup.com>
2023-07-24 17:46:41 +0200

    allow cors

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ const jsonHTMLClient = new JsonHtml();
 // App
 
 const app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+     next();
+});
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
